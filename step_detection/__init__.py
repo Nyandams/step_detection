6c5424e4afec78b8c5b5019e60052536ff05f3d5
Damien Lecha <lecha.damien@gmail.com>
2020-04-21 16:13:40 +0200

    add group_steps to __init__.py

--- step_detection/__init__.py
+++ step_detection/__init__.py
@@ -1,2 +1,3 @@
 from .detection import (EscalatorRush)
 from .costs import (DistanceMinMaxCost, DistanceMedianCost, DistanceMeanCost)
+from .base import group_steps
--- step_detection/base.py
+++ step_detection/base.py
@@ -1,5 +1,7 @@
 import abc
 from typing import List, Tuple, Any
+import numpy as np
+
 
 class BaseCost(metaclass=abc.ABCMeta):
     @classmethod
@@ -41,3 +43,31 @@ class BaseEstimator(metaclass=abc.ABCMeta):
     def predict(self, *args, **kwargs):
         """To call the segmentation algorithm"""
         raise NotImplementedError
+
+
+def group_steps(steps: List[Tuple[int, int, Any]], signal, gap: int = 3, dist: int = 2):
+    """
+    Regroup 2 steps together
+    :param steps:
+    :param gap: distance in 'time' between 2 steps (maximum index difference between 2 steps)
+    :param dist: distance allowed between 2 values of steps
+    :return: a new list of steps  List[Tuple[int, int, Any]]
+    """
+    i: int = 0
+    grouped_step: List[Tuple[int, int, Any]] = []
+    precedent = None
+    while i < len(steps):
+        print(precedent)
+        if not precedent:
+            precedent = steps[i]
+        elif not precedent and i == len(steps)-1:
+            print("yep")
+            grouped_step.append(steps[i])
+        elif steps[i][0] - precedent[1] < gap and abs(steps[i][2] - precedent[2]) < dist:
+            precedent = (precedent[0], steps[i][1], np.mean(signal[precedent[0]:steps[i][1]]))
+        else:
+            grouped_step.append(precedent)
+            precedent = steps[1]
+        i += 1
+
+    return grouped_step
